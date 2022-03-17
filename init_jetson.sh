f0ee2f7958ae4ffa676d723ffcb8d5aa69747b4b
Loc Chuong <locchuong123@gmail.com>
2022-03-17 14:33:24 +0700

    Add files via upload

---
+++ init_jetson.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# install nano 
+sudo apt-get update
+sudo apt-get install nano
+echo "GNU nano installed!"
+
+# install python3
+sudo apt update
+sudo apt-get install python python3
+echo "python3 installed!"
+
+# install pip3
+sudo apt update
+sudo apt-get install python3-pip
+echo "pip3 installed!"
+
+# install virtualenv
+sudo apt update
+sudo apt-get install virtualenv
+echo "virtualenv installed"
+
+# install jupyter
+sudo apt update
+sudo apt-get install jupyter
+echo "jupyter installed"
+
+# install tensorflow
+
+echo "tensorflow installed"
+
+# install pytorch
+
+echo "pytorch installed"
+
+# update all
+
+echo "All updated!"
+
+# reboot your system
+
+echo "Reboot your system now!"
